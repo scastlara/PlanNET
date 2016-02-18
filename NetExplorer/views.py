4bd4d9e8957357c338b6214ebd4ea1fd1aeac522
Sergio Castillo <s.cast.lara@gmail.com>
2016-02-18 11:54:15 +0100

    Two new templates.

--- NetExplorer/views.py
+++ NetExplorer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+# -----------------------
+# VIEWS
+# -----------------------
+
+def index_view(request):
+    '''
+    This is the first view the user sees.
+    It contains the forms, a mini-tutorial, etc.
+    '''
+    return render(request, 'NetExplorer/index.html', {'msg': "HELLO WORLD"} )
