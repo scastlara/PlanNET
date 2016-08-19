b3416fa0a866d62c7d4f004cde435b54e6198708
Sergio Castillo <s.cast.lara@gmail.com>
2016-08-19 11:59:13 +0200

    Skeleton of the app

--- NetExplorer/urls.py
+++ NetExplorer/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index_view),
+    url(r'^$', views.index_view, name='index_view'),
+    url(r'^gene_searcher', views.gene_searcher, name="gene_searcher"),
 ]
--- NetExplorer/views.py
+++ NetExplorer/views.py
@@ -10,5 +10,29 @@ def index_view(request):
     '''
     This is the first view the user sees.
     It contains the forms, a mini-tutorial, etc.
+    And links to all the functions.
     '''
-    return render(request, 'NetExplorer/index.html', {'msg': "HELLO WORLD"} )
+    return render(request, 'NetExplorer/index.html')
+
+
+
+def gene_searcher(request):
+    '''
+    This is the text-based database search function.
+    '''
+    if request.method == "GET" and "genesymbol" in request.GET:
+        genesymbol = request.GET['genesymbol']
+        database   = request.GET['database']
+        msg = "You searched for "
+        if genesymbol:
+            msg = msg + genesymbol + " in %s" % database
+        else:
+            msg = msg + "nothing"
+        return render(request, 'NetExplorer/gene_searcher.html', {'msg': msg} )
+    else:
+        return render(request, 'NetExplorer/gene_searcher.html', {'msg': "HELLO WORLD"} )
+
+def net_explorer(request):
+    '''
+    This is the cytoscape graph-based search function.
+    '''
