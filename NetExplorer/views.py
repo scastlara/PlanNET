1a6a666072dcf7146238f59b0c2d32d7ca295e48
Sergio Castillo <s.cast.lara@gmail.com>
2016-08-19 13:30:50 +0200

    Now makes a query to Neo4j

--- NetExplorer/views.py
+++ NetExplorer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from py2neo import Graph, Path
 
 # -----------------------
 # VIEWS
@@ -12,6 +13,7 @@ def index_view(request):
     It contains the forms, a mini-tutorial, etc.
     And links to all the functions.
     '''
+
     return render(request, 'NetExplorer/index.html')
 
 
@@ -21,14 +23,29 @@ def gene_searcher(request):
     This is the text-based database search function.
     '''
     if request.method == "GET" and "genesymbol" in request.GET:
+        graph = Graph("http://localhost:7474/db/data/")
+
         genesymbol = request.GET['genesymbol']
         database   = request.GET['database']
-        msg = "You searched for "
+
+        msg     = "You searched for "
+        symbols = list()
+
         if genesymbol:
+            results = graph.run("MATCH (n:%s) WHERE n.symbol = '%s' RETURN n.symbol AS symbol LIMIT 1" % (database, genesymbol))
+            for row in results:
+                symbols.append(row['symbol'])
             msg = msg + genesymbol + " in %s" % database
+
+            if symbols:
+                return render(request, 'NetExplorer/gene_searcher.html', {'msg': msg, 'res': symbols } )
+            else:
+                return render(request, 'NetExplorer/gene_searcher.html', {'msg': msg, 'res': symbols, 'search_error': 1 } )
         else:
             msg = msg + "nothing"
-        return render(request, 'NetExplorer/gene_searcher.html', {'msg': msg} )
+
+        # Render when user enters the page
+        return render(request, 'NetExplorer/gene_searcher.html', {'msg': msg } )
     else:
         return render(request, 'NetExplorer/gene_searcher.html', {'msg': "HELLO WORLD"} )
 
