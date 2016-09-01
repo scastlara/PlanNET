bf2fd881ac5b807dee3d891f703cb5b7294ea9af
Sergio Castillo <s.cast.lara@gmail.com>
2016-09-01 16:48:17 +0200

    Now back button working thanks to history.pushState

--- NetExplorer/views.py
+++ NetExplorer/views.py
@@ -60,14 +60,16 @@ def get_fasta(request):
         pass # server error!
 
     sequence = str()
+    filename = "%s" % node.symbol
     if typeseq == "sequence":
         sequence = textwrap.fill(node.sequence, 70)
+        filename = filename + ".fa"
     elif typeseq == "orf":
         sequence = textwrap.fill(node.orf, 70)
+        filename = filename + "-orf.fa"
 
     sequence = ">%s\n" % node.symbol + sequence
     response = HttpResponse(sequence, content_type='text/plain; charset=utf8')
-    filename = "%s.fa" % node.symbol
     response['Content-Disposition'] = 'attachment; filename=%s' % filename
     return response
 
