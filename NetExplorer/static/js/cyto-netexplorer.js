c8d4f00df3e7b2e177d0df5aa6645e8f622cc7c3
Sergio Castillo <s.cast.lara@gmail.com>
2016-09-28 12:48:40 +0200

    Added cards on netexplorer when click on node. Bug fix: now only loads bootstrap and jquery once in gene_card.

--- NetExplorer/static/js/cyto-netexplorer.js
+++ NetExplorer/static/js/cyto-netexplorer.js
@@ -12,7 +12,6 @@ var stylesheet = cytoscape.stylesheet()
         })
     .selector('edge')
         .css({
-            'target-arrow-shape': 'triangle',
             'width': 1,
             'line-color': 'data(colorEDGE)',
             'target-arrow-color': 'data(colorEDGE)'
@@ -35,6 +34,43 @@ cy.load(graphelements);
 
 // Change layout control
 $('#select-layout li').on('click', function(){
-    var newlayout = $(this).text();
+    var newlayout = $(this).text().toLowerCase();
     cy.layout( { name: newlayout } )
+});
+
+$(".dropdown-menu li a").click(function(){
+  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+});
+
+
+
+// Info card on click
+cy.nodes().on("click", function(){
+    var card_data = {
+        target  : this.data("name"),
+        targetDB: this.data("database"),
+        csrfmiddlewaretoken: '{{ csrf_token }}'
+    }
+
+
+    // Get the ID of the div to update
+    elementID = "card-overlay";
+    $.ajax({
+        type: "GET",
+        url: "/info_card",
+        data: {
+            'target'    : card_data['target'],
+            'targetDB'  : card_data['targetDB'],
+            'csrfmiddlewaretoken': '{{ csrf_token }}'
+        },
+        success : function(data) {
+            $('[id="' + elementID + '"]').html(data);
+            $('[id="card-overlay"]').slideToggle(450);
+            $('.close-overlay').slideToggle(450);
+            $('.full-screen-card').slideToggle(450);
+        }
+    });
+
+
 });
