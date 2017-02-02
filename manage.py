ed3c3c9cd9ba8b35cff338cfd35a43efbd34f92f
Sergio Castillo <s.cast.lara@gmail.com>
2017-02-02 12:50:07 +0100

    MANAGE FOR PRODUCTION

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PlaNET.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PlanNET.settings")
 
     from django.core.management import execute_from_command_line
 
