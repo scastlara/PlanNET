c6d0578de3ce8a52a0c496c82ad4f0b955036063
Sergio Castillo <s.cast.lara@gmail.com>
2017-03-13 17:43:54 +0100

    Warning when too many nodes!

--- static/js/netexplorer-countnodes.js
+++ static/js/netexplorer-countnodes.js
@@ -2,5 +2,8 @@
 function countNodes(cyto) {
     var nodes = cyto.elements('node[database != "Human"]');
     var count = nodes.length;
+    if (count >= 150) {
+        count = "<span class='text-danger'>" + count + " <span class='glyphicon glyphicon-warning-sign'></span></span>";
+    }
     $("#node-count").html(count);
 }
