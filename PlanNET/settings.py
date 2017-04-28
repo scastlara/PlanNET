4af8fc7b29a2273d34d315a8735758f2459e7635
Sergio Castillo <s.cast.lara@gmail.com>
2017-04-28 17:04:24 +0200

    New settings.py for production. Removed DEBUG=True.

--- PlanNET/settings.py
+++ PlanNET/settings.py
@@ -4,7 +4,7 @@ import logging
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     ('WebAdmin', 'compgen@ub.edu'),
@@ -33,7 +33,7 @@ logging.basicConfig(
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["compgen.bio.ub.edu"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
