68b85b7772c3b698b308b233956838966c6fc745
Sergio Castillo <s.cast.lara@gmail.com>
2017-06-19 15:25:41 +0200

    Ask user before loading json to netexplorer with >100 nodes

--- static/js/netexplorer-addnode.js
+++ static/js/netexplorer-addnode.js
@@ -1,6 +1,37 @@
 // ADD NODE WHEN CLICKING ON NODE IN CYTOSCAPE JS
 // Function to check if the toggle "show homologs is On or Off and do whatever needs to be done"
 
+function loadJsonGraph(data, cyobj) {
+    // Function that takes the json and the cyobj and loads the graph
+    var newelements = cyobj.add(data);
+    checkHomologs(document.getElementById( "show-homologs" ), cyobj); // Show homologs if necessary
+
+    cyobj.layout({
+        name: 'cola',
+        maxSimulationTime: 3000,
+        fit: true,
+        directed: false,
+        padding: 40
+    });
+    $('#loading').hide();
+
+    // Show only edges above slider threshold
+    var value = $('#sl1').val();
+    cyobj.filter(function(i, element){
+        if ( element.isEdge() ) {
+            if( element.data("probability") >= value ){
+                element.show();
+                return true;
+            }
+            element.hide();
+        }
+        // Not an edge
+    });
+    // Show or hide edges depending on plen
+    checkPlen($('input[name=show-plen]:checked').val(), cyobj);
+    countNodes(cyobj);
+}
+
 function addNode(symbol, database, type, cyobj) {
     // This function takes a symbol and a database and adds the node's neighbour nodes
     // to the cytoscape object. It does an ajax request to net_explorer to get the
@@ -20,32 +51,33 @@ function addNode(symbol, database, type, cyobj) {
         },
         success : function(data) {
             var layout_name = $('#select-layout li').text().toLowerCase();
-            var newelements = cyobj.add(data);
-            checkHomologs(document.getElementById( "show-homologs" ), cyobj); // Show homologs if necessary
-            cyobj.layout({
-                name: 'cola',
-                maxSimulationTime: 3000,
-                fit: true,
-                directed: false,
-                padding: 40
-            });
-            $('#loading').hide();
+            if (data.nodes.length >= 100) {
+                // You are adding too many nodes, ask before adding nodes
+                $("#toomany-number").html(data.nodes.length);
 
-            // Show only edges above slider threshold
-            var value = $('#sl1').val();
-            cyobj.filter(function(i, element){
-                if ( element.isEdge() ) {
-                    if( element.data("probability") >= value ){
-                        element.show();
-                        return true;
+                $( "#dialog-toomany" ).dialog({
+                    resizable: false,
+                    height: "auto",
+                    width: 400,
+                    modal: true,
+                    buttons: {
+                        Cancel: function() {
+                            $( this ).dialog( "close" );
+                            $('#loading').hide();
+                            return;
+                        },
+                        "Add nodes": function() {
+                            loadJsonGraph(data, cyobj);
+                            $( this ).dialog( "close" );
+                            $('#loading').hide();
+                        }
                     }
-                    element.hide();
-                }
-                // Not an edge
-            });
-            // Show or hide edges depending on plen
-            checkPlen($('input[name=show-plen]:checked').val(), cyobj);
-            countNodes(cyobj);
+                });
+            } else {
+                // Add nodes no matter what
+                loadJsonGraph(data, cyobj);
+                $('#loading').hide();
+            }
         },
         statusCode: {
             404: function () {
