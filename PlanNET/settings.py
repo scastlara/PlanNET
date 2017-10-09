2c4f48cebbc2fdb0b124c74a090082e8bfb68450
Sergio Castillo <s.cast.lara@gmail.com>
2017-10-09 11:39:53 +0200

    Settings mysql db for production.

--- PlanNET/settings.py
+++ PlanNET/settings.py
@@ -14,11 +14,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db_plannet',                      # Or path to database file if using sqlite3.
+        'USER': 'wma',                      # Not used with sqlite3.
+        'PASSWORD': 'k1k1cantava1aRANA',                  # Not used with sqlite3.
+        'HOST': '161.116.70.62',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
