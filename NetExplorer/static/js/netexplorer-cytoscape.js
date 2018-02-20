868eed0b30f09a736bb04597c0683bbe61760596
Sergio Castillo <s.cast.lara@gmail.com>
2018-02-20 10:15:18 +0100

    Search-node separated

--- NetExplorer/static/js/netexplorer-cytoscape.js
+++ NetExplorer/static/js/netexplorer-cytoscape.js
@@ -129,35 +129,6 @@ $('#sl1').slider('setValue', defaultSliderValue);
 
 
 // --------------------------
-// SEARCH NODE
-    $("#search-netexplorer-btn").on("click", function(){
-        var searchterm = $("#genename-graph").val();
-        if (searchterm) {
-            var selectorName = 'node[name @="' + searchterm + '"]';
-            var selectorHomologs = 'node[homolog @="' + searchterm + '"]';
-
-            var theElements = cy.elements(selectorName);
-            if (theElements.length) {
-                // Match by Name
-                theElements.addClass('highlighted');
-            } else {
-                theElements = cy.elements(selectorHomologs);
-                if (theElements.length) {
-                    // Match by Homologs
-                    theElements.addClass('highlighted');
-                } else {
-                    // No match
-                    alert("NO MATCH!");
-                }
-            }
-        }
-    });
-
-    $("#removesearch").on("click", function(){
-        cy.nodes().removeClass('highlighted');
-    });
-
-// --------------------------
 // CYTOSCAPE CONTROLS
 
     $(".btn").mouseup(function(){
