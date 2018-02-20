a114e2e867d874279e16d4c98236611c5172d961
Sergio Castillo <s.cast.lara@gmail.com>
2018-02-20 15:38:58 +0100

    Multiple search of genes separated by commas

--- NetExplorer/static/js/search-node.js
+++ NetExplorer/static/js/search-node.js
@@ -1,29 +1,22 @@
 
 // --------------------------
 // SEARCH NODE
-    $("#search-netexplorer-btn").on("click", function(){
-        var searchterm = $("#genename-graph").val();
-        if (searchterm) {
-            var selectorName = 'node[name @="' + searchterm + '"]';
-            var selectorHomologs = 'node[homolog @="' + searchterm + '"]';
-
-            var theElements = cy.elements(selectorName);
-            if (theElements.length) {
-                // Match by Name
-                theElements.addClass('highlighted');
+$("#search-netexplorer-btn").on("click", function(){
+    var searchterm = $("#genename-graph").val();
+    var terms = searchterm.split(",").map(function(x){ return x.toUpperCase()});
+    if (terms.length) {
+        cy.nodes().filter(function(eidx, ele) {
+            if (terms.indexOf(ele.data("name").toUpperCase()) !== -1) {
+                return true;
+            } else if (terms.indexOf(ele.data("homolog").toUpperCase()) !== -1 ) {
+                return true;
             } else {
-                theElements = cy.elements(selectorHomologs);
-                if (theElements.length) {
-                    // Match by Homologs
-                    theElements.addClass('highlighted');
-                } else {
-                    // No match
-                    alert("NO MATCH!");
-                }
+                return false;
             }
-        }
-    });
+        }).addClass("highlighted");
+    }
+});
 
-    $("#removesearch").on("click", function(){
-        cy.nodes().removeClass('highlighted');
-    });
+$("#removesearch").on("click", function(){
+    cy.nodes().removeClass('highlighted');
+});
