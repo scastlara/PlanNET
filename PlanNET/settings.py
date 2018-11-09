24441de0bec3b80fbdc2e4ded57fd1840694bf0e
scastlara <s.cast.lara@gmail.com>
2018-11-09 15:04:47 +0100

    DEVELOPMENT ONLY: settings.py for admin page.

--- PlanNET/settings.py
+++ PlanNET/settings.py
@@ -138,10 +138,11 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'NetExplorer',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+     'django.contrib.admindocs',
 )
+AUTH_USER_MODEL = 'NetExplorer.User'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
