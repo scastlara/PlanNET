d939730a43ec6228b207e3bdf2398eb599c6ac1b
scastlara <s.cast.lara@gmail.com>
2019-05-07 15:31:40 +0200

    Making PlanExp public

--- NetExplorer/views/planexp.py
+++ NetExplorer/views/planexp.py
@@ -1,6 +1,6 @@
 from .common import *
 
-@login_required
+
 def planexp(request):
     experiments = Experiment.get_allowed_experiments(request.user)
     return render(request, 'NetExplorer/planexp.html', { 'experiments': experiments })
